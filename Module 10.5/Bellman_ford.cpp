55d9646521164aba24786b9691c82e8675ff6fee
Rafa <ramijahsultana@gmail.com>
2025-09-06 00:55:57 +0600

    Union

--- Module 10.5/Bellman_ford.cpp
+++ Module 10.5/Bellman_ford.cpp
@@ -1,15 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+class Edge
+{
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+
+vector<Edge> edge_list;
+int n, e;
+int dis[105];
+
 int main()
 {
-    int n, e;
+
     cin >> n >> e;
-    vector<int, pair<int, int>> egde_list;
-    while(e--)
+    vector<Edge> edge_list;
+    while (e--)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        egde_list.push_back({a,b,c});
+        edge_list.push_back(Edge(a, b, c));
     }
+
+    for (int i = 0; i < n; i++)
+        dis[i] = INT_MAX;
+    dis[0] = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+
+        for (auto ed : edge_list)
+        {
+            int a, b, c;
+            a = ed.a;
+            b = ed.b;
+            c = ed.c;
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+                dis[b] = dis[a] + c;
+        }
+    }
+    bool cycle = false;
+    for (auto ed : edge_list)
+    {
+        int a, b, c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+        if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            cycle = true;
+    }
+    if (cycle)
+    {
+        cout << "-1" << endl;
+    }
+    else
+        for (int i = 0; i < n; i++)
+            cout << dis[i] << " ";
     return 0;
 }
